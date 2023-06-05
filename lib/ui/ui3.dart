795e7f5a1250c65dbc2ed1af613df6e8e0b73ea5
Soliyev909 <soliyev909@gamil.com>
2023-06-05 15:42:44 +0500

    commit

--- lib/ui/ui3.dart
+++ lib/ui/ui3.dart
@@ -1,63 +1,52 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp( const CupertinoApp(
-    debugShowCheckedModeBanner: false,
-    home: Page1()
-  ));
-}
-
-class Page1 extends StatelessWidget{
-
-  const Page1({super.key});
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Container(
-            margin: const EdgeInsets.all(5),
-            height: 700,
-            width: 368,
-            color: Colors.indigo,
-            alignment: Alignment.center,
-            child:Container(
-              height: 685,
-              width: 335,
-              color: Colors.white,
-              alignment: Alignment.topCenter,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(top: 40),
-                    height: 150,
-                    width: 320,
-                    color: Colors.black,
-                    alignment: Alignment.center,
-                    child: Container(
-                      height: 137,
-                      width: 306,
-                      color: Colors.white,
-                      child: const Text("Fixed height container"),
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(top:20),
-                    height: 470,
-                    width: 320,
-                    decoration: BoxDecoration(
-                      border: Border.all(width: 7,color: Colors.purple)
-                    ),
-                    child: Container(
-                      height: 495,
-                      width: 306,
-                      padding: const EdgeInsets.all(5),
-                      color: Colors.white,
+void main() {
+  runApp(
+    CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Container(
+              margin: const EdgeInsets.all(5),
+              height: 700,
+              width: 368,
+              color: Colors.indigo,
+              alignment: Alignment.center,
+              child: Container(
+                height: 685,
+                width: 335,
+                color: Colors.white,
+                alignment: Alignment.topCenter,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(top: 40),
+                      height: 150,
+                      width: 320,
+                      color: Colors.black,
                       alignment: Alignment.center,
+                      child: Container(
+                        height: 137,
+                        width: 306,
+                        color: Colors.white,
+                        child: const Text("Fixed height container"),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(top: 20),
+                      height: 470,
+                      width: 320,
+                      decoration: BoxDecoration(
+                          border: Border.all(width: 7, color: Colors.purple)),
+                      child: Container(
+                        height: 495,
+                        width: 306,
+                        padding: const EdgeInsets.all(5),
+                        color: Colors.white,
+                        alignment: Alignment.center,
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
@@ -70,20 +59,21 @@ class Page1 extends StatelessWidget{
                                   height: 400,
                                   width: 160,
                                   decoration: BoxDecoration(
-                                    border: Border.all(width: 7,color: Colors.red)
-                                  ),
+                                      border: Border.all(
+                                          width: 7, color: Colors.red)),
                                   child: const Text("""Expanded chart"""),
                                 ),
                               ],
                             ),
                             Container(
-                             margin: const EdgeInsets.only(top: 10),
-                             height: 450,
-                             width: 130,
-                             decoration: BoxDecoration(
-                               border: Border.all(width: 7,color: Colors.black),
-                             ),
-                             alignment: Alignment.center,
+                              margin: const EdgeInsets.only(top: 10),
+                              height: 450,
+                              width: 130,
+                              decoration: BoxDecoration(
+                                border:
+                                    Border.all(width: 7, color: Colors.black),
+                              ),
+                              alignment: Alignment.center,
                               child: Container(
                                 height: 423,
                                 width: 140,
@@ -91,17 +81,18 @@ class Page1 extends StatelessWidget{
                                 alignment: Alignment.center,
                                 child: const Text("Fixed width container"),
                               ),
-                              ),
+                            ),
                           ],
                         ),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
         ),
       ),
-    );
-  }
+    ),
+  );
 }
